e08141d3942cbc527ac0fd5eb61110683606730a
naveenreddysg <naveenreddy.sg@gmail.com>
2018-06-29 16:34:17 +0530

    make it better

--- app.py
+++ app.py
@@ -125,7 +125,9 @@ if __name__ == '__main__':
   # When running locally, disable OAuthlib's HTTPs verification.
   # ACTION ITEM for developers:
   #     When running in production *do not* leave this option enabled.
-  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
   # Specify a hostname and port that are set as a valid redirect URI
   # for your API project in the Google API Console.
-  app.run('localhost', 8080, debug=True)
+
+  port = int(os.environ.get("PORT", 8000))
+  app.run(host="localhost", debug=True, port=port)
