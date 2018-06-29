47d860423a781f18c6a15bd45846c41faf12dcad
naveenreddysg <naveenreddy.sg@gmail.com>
2018-06-29 16:32:58 +0530

    make it better

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def oauth2callback():
   credentials = flow.credentials
   flask.session['credentials'] = credentials_to_dict(credentials)
 
-  return flask.redirect(flask.url_for('test_api_request'))
+  return flask.redirect(flask.url_for('index'))
 
 
 @app.route('/revoke')
@@ -125,7 +125,7 @@ if __name__ == '__main__':
   # When running locally, disable OAuthlib's HTTPs verification.
   # ACTION ITEM for developers:
   #     When running in production *do not* leave this option enabled.
+  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
   # Specify a hostname and port that are set as a valid redirect URI
   # for your API project in the Google API Console.
-
   app.run('localhost', 8080, debug=True)
