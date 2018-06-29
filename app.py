3e144b4adc916153235c05c61511127702ccfa8d
naveenreddysg <naveenreddy.sg@gmail.com>
2018-06-29 16:23:34 +0530

    make it better

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import os
 import flask
 import requests
@@ -8,12 +6,8 @@ import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 
-# This variable specifies the name of a file that contains the OAuth 2.0
-# information for this application, including its client_id and client_secret.
 CLIENT_SECRETS_FILE = "client_secret.json"
 
-# This OAuth 2.0 access scope allows for full read/write access to the
-# authenticated user's account and requires requests to use an SSL connection.
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 API_SERVICE_NAME = 'analytics'
 API_VERSION = 'v3'
@@ -71,21 +65,15 @@ def authorize():
 
 @app.route('/oauth2callback')
 def oauth2callback():
-  # Specify the state when creating the flow in the callback so that it can
-  # verified in the authorization server response.
   state = flask.session['state']
 
   flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
       CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
   flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
 
-  # Use the authorization server's response to fetch the OAuth 2.0 tokens.
   authorization_response = flask.request.url
   flow.fetch_token(authorization_response=authorization_response)
 
-  # Store credentials in the session.
-  # ACTION ITEM: In a production app, you likely want to save these
-  #              credentials in a persistent database instead.
   credentials = flow.credentials
   flask.session['credentials'] = credentials_to_dict(credentials)
 
@@ -124,5 +112,5 @@ def credentials_to_dict(credentials):
 if __name__ == '__main__':
 
   os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-  port = int(os.environ.get("PORT", 8000))
+  port = int(os.environ.get("PORT", 8080))
   app.run(host="localhost", debug=True, port=port)
