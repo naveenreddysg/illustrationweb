f7e6f92b6fc70005d9283bb7a24e1e4ba626160d
naveenreddysg <naveenreddy.sg@gmail.com>
2018-06-29 16:48:08 +0530

    make it better

--- app.py
+++ app.py
@@ -122,11 +122,12 @@ def credentials_to_dict(credentials):
 
 
 if __name__ == '__main__':
+
   # When running locally, disable OAuthlib's HTTPs verification.
   # ACTION ITEM for developers:
   #     When running in production *do not* leave this option enabled.
 
-  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+  # os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
   # Specify a hostname and port that are set as a valid redirect URI
 
